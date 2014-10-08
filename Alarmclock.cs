16a1551e8c07fa36bb6e3969b60a27bd0cfe4e84
Martin Arvidsson UD14 <ma223ku@student.lnu.se>
2014-10-08 16:02:45 +0200

    Påbörjat C labben.

---
+++ Alarmclock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalVäckarklocka
+{
+    class Alarmclock
+    {
+        private int _AlarmclockHour;
+        private int _AlarmclockMinute;
+        private int _Hour;
+        private int _Minute;
+    }
+}
